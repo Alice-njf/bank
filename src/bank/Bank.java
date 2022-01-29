6d1960e04425782ba1e6282eb88bae85f49b8c0f
Alice-njf <84927344+Alice-njf@users.noreply.github.com>
2022-01-30 00:34:51 +0100

    Update Bank.java

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -11,11 +11,15 @@ public class Bank {
 		obj1.msg = "test";
 		obj1.text = "ftre";
 		obj2.balance = 2000;
-		obj2.name = "William";
+		obj2.name = "Jo";
+		obj4.balance = 9000;
+		obj4.name = "William";
 		obj1.withdrow("Withdrow", 100);
 		obj1.checkBalance("Balance", 100);
 		obj2.withdrow("Withdrow", 100);
 		obj2.checkBalance("Balance", 10);
+		obj4.withdrow("Withdrow", 100);
+		obj4.checkBalance("Balance", 10);
 		System.out.println("End");
 	}
 }
